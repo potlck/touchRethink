0f0a6a12fd7df78bfacdd68fda81847d25a47ed0
Richard Higgins <aqwin@heron.(none)>
2014-08-03 12:39:54 -0700

    refactored

--- updateNodeConf.rb
+++ updateNodeConf.rb
@@ -20,7 +20,7 @@ r.table('nodes').changes().filter{|row| # catches only changes that add/delete n
 		config += li
   		if (li['upstream cluster {'])
   			puts "found beginning of config section: add an ip address here"
-  			config += "server #{ipToAdd}:1995;"
+  			config += "server #{ipToAdd}:1995;\n"
   		end
 	end
 
@@ -31,8 +31,28 @@ r.table('nodes').changes().filter{|row| # catches only changes that add/delete n
 	
 	# if NEW:
 	exec "echo #{ipToAdd}"
-	exec "git clone <repo> #{idToAdd}"
-	exec "git pull <repo> #{idToAdd}"
-	exec "chown 9999:9999 -R /home/app/#{idToAdd}"
+	exec "git clone <repo> #{id}"
+	exec "git pull <repo> #{id}"
+	exec "chown 9999:9999 -R /home/app/#{id}"
 
 }
+
+
+#
+#if (change["new_val"] == nil)
+#		config = ""
+#		
+#		conf = File.open('/etc/nginx/sites-enabled/app.conf').read
+#		conf.each_line do |li|
+#	  		if (!(li["server #{ipToAdd}:1995"]))
+#	  			config += li
+#	  		else
+#	  			puts "found line to remove ip"
+#	  		end
+#		end
+
+		# edit config
+#		File.write('/etc/nginx/sites-enabled/app.conf', config)
+#		# reload nginx
+#		exec '/etc/init.d/nginx reload'
+#	else
